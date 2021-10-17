f3786f1f30907447e92c32de068072ab745f3719
Baltasar del Sol de Haro <baltasardelsol@gmail.com>
2021-10-17 15:48:58 +0200

    Cambios: Eliminada clase Stock La clase producto ahora tiene un atributo stock
    Closes #17

--- src/Product.js
+++ src/Product.js
@@ -1,10 +1,25 @@
 class Product {
 	#id;
 	#name;
+	#stock;
 
-	constructor(id, name){
-		this.id = id;
-		this.name = name;
+	constructor(product, quantity){
+		this.#id = id;
+		this.#name = name;
+		this.#stock= stock;
+	}
+
+	addQuantity(amount){
+		this.#stock += amount;
+	}
+
+	substractQuantity(amount){
+		if (this.#stock <= 0) {
+			this.#stock = 0;
+			throw 'Spent more than was available!';
+		}
+
+		this.#stock -= amount;
 	}
 }
 
