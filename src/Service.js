7b37edbc357c335e87bdaf27b78c4fe21133c0c5
Baltasar del Sol de Haro <baltasardelsol@gmail.com>
2021-10-25 17:50:43 +0200

    Related to #15: Fixed class Service

--- src/Service.js
+++ src/Service.js
@@ -6,14 +6,12 @@ class Service{
 	#id;
 	#name;
 	#consumption;
-	#time;
 	#price;
 	
-	constructor(name, consumption, time, price) {
+	constructor(name, consumption, price) {
 		this.#id = this.#idCounter;
 		this.#name = name;
 		this.#consumption = consumption;		// Consumption is an array that constains a struct with a product and the quantity used
-		this.#time = time;
 		this.#price = price;
 
 		this.#idCounter++;
