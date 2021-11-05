7182bcd19a81ef2d48ae7a3a42ec36c53e21810b
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-11-05 12:39:55 +0100

    desarrollo las tareas #24

--- src/Service.js
+++ src/Service.js
@@ -1,22 +1,11 @@
-import { Product } from './Product';
-
-
 class Service{
-	static #idCounter = 0;
-	#id;
-	#name;
-	#consumption;
-	#price;
-	
 	constructor(name, consumption, price) {
-		this.#id = this.#idCounter;
-		this.#name = name;
-		this.#consumption = consumption;		// Consumption is an array that constains a struct with a product and the quantity used
-		this.#price = price;
-
-		this.#idCounter++;
+		this.name = name;
+		this.consumption = consumption;		// Consumption is an array that constains a struct with a product and the quantity used
+		this.price = price;
+	}
+	getName(){
+		return this.name;
 	}
 }
-
-
-export { Service };
+module.exports = Service;
