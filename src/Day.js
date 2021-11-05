d8c05e8293a54b684ada27969350b26977c037a3
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-11-05 12:34:16 +0100

    desarrollo las tareas #21 y #23

--- src/Day.js
+++ src/Day.js
@@ -1,48 +1,35 @@
-import { Service } from './Service';
-
-
-class Appointment{
-	#service;
-	#startTime;
-	#customerName;
-
-	constructor(service, startTime, customerName){
-		this.#service = service;
-		this.#startTime = startTime;
-		this.#customerName = customerName;
-	}
-}
-
-
 class Day{
-	#date;
-	#appointments = [];
-
 	constructor(date){
-		this.#date = date;
+		this.date = date;
+		this.hours = {};
 		let dayOfTheWeek = date.getDay();
 
-		this.#appointments = []
 		switch (dayOfTheWeek) {
-			case 'Lunes':
-				Horario = {'18:00':false,'18:30':false,'19:00':false,'19:30':false,'20:00':false,'20:30':false}
+			case 1:
+				this.hours = {'18:00':false,'18:30':false,'19:00':false,'19:30':false,'20:00':false,'20:30':false}
 				break;
 				
-			case 'Martes':
-			case 'Miercoles':
-			case 'Jueves':
-			case 'Viernes':
-			case 'Sabado':
-				Horario = {'08:00':false,'08:30':false,'09:00':false,'09:30':false,'10:00':false,'10:30':false,'11:00':false,'11:30':false,'12:00':false,'12:30':false,'18:00':false,'18:30':false,'19:00':false,'19:30':false,'20:00':false,'20:30':false}
-				break;
-			
-			case 'Domingo':
-				Horario = []
+			case 2:
+			case 3:
+			case 4:
+			case 5:
+			case 6:
+				this.hours = {'08:00':false,'08:30':false,'09:00':false,'09:30':false,'10:00':false,'10:30':false,'11:00':false,'11:30':false,'12:00':false,'12:30':false,'18:00':false,'18:30':false,'19:00':false,'19:30':false,'20:00':false,'20:30':false}
 				break;
 		}
 	}
-
+	hourReservated(hour){
+        if(!this.hours[hour]){
+            return false
+        }
+        return true
+    }
+	getDateString(){
+		let mes = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+        return this.date.getDate() + ' de ' + mes[this.date.getMonth()] + ' de ' + this.date.getFullYear()
+    }
+	getHours(){
+		return this.hours;
+	}
 }
-
-
-export { Day };
+module.exports = Day;
