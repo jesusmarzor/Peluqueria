dbcbd8d426ca9a4785e4125177f2ec1dbcdbb643
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-11-06 21:55:54 +0100

    creo los tests para desarrollar la tarea #31 y resolver la [HU1]#3

--- test/App.test.js
+++ test/App.test.js
@@ -1,7 +1,10 @@
 const App = require('../src/App');
 const app = new App();
+const calendar = app.getCalendar();
 const products = app.getProducts();
 const services = app.getServices();
+const day = calendar.getTimetable()[25];
+const hour = '18:00';
 describe('M1 - Sistema de Citas', () => {
     test('Añadir Productos', ()=>{
         app.addProducts('acondicionador',5,50);
@@ -16,5 +19,25 @@ describe('M1 - Sistema de Citas', () => {
         expect(services[0].getConsumption()).toEqual([[products[0],15]]);
         expect(services[0].getPrice()).toBe(8);
         expect(services.length).toBe(1);
+    })
+    test('Pedir cita', () => {
+        app.giveAppointment('Jesus',day,hour,services[0]);
+        let appointments = app.getAppointments(day);
+        let appointmentGiven = appointments[hour];
+        expect(appointmentGiven.getCustomerName()).toBe('Jesus');
+        expect(appointmentGiven.getService()).toEqual(services[0]);
+    })
+    test('Pedir cita a una hora que está cerrado', () => {
+        app.giveAppointment('Juan',day,'22:00',services[0]);
+        let appointments = app.getAppointments(day);
+        let appointmentGiven = appointments['22:00'];
+        expect(appointmentGiven).toBe(undefined);
+    })
+    test('Pedir cita a una hora que está ocupada', () => {
+        app.giveAppointment('Juan',day,hour,services[0]);
+        let appointments = app.getAppointments(day);
+        let appointmentGiven = appointments[hour];
+        expect(appointmentGiven.getCustomerName()).toBe('Jesus');
+        expect(appointmentGiven.getService()).toEqual(services[0]);
     })
 })
