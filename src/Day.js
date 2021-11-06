6bc06f6c69be2ff0306043d8a99dd42b137e25aa
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-11-06 19:32:02 +0100

    desarrollo la tarea #28

--- src/Day.js
+++ src/Day.js
@@ -1,12 +1,13 @@
 class Day{
 	constructor(date){
 		this.date = date;
-		this.hours = {};
+		this.appointments = {};
+
 		let dayOfTheWeek = date.getDay();
 
 		switch (dayOfTheWeek) {
 			case 1:
-				this.hours = {'18:00':false,'18:30':false,'19:00':false,'19:30':false,'20:00':false,'20:30':false}
+				this.appointments = {'18:00':false,'18:30':false,'19:00':false,'19:30':false,'20:00':false,'20:30':false}
 				break;
 				
 			case 2:
@@ -14,12 +15,12 @@ class Day{
 			case 4:
 			case 5:
 			case 6:
-				this.hours = {'08:00':false,'08:30':false,'09:00':false,'09:30':false,'10:00':false,'10:30':false,'11:00':false,'11:30':false,'12:00':false,'12:30':false,'18:00':false,'18:30':false,'19:00':false,'19:30':false,'20:00':false,'20:30':false}
+				this.appointments = {'08:00':false,'08:30':false,'09:00':false,'09:30':false,'10:00':false,'10:30':false,'11:00':false,'11:30':false,'12:00':false,'12:30':false,'18:00':false,'18:30':false,'19:00':false,'19:30':false,'20:00':false,'20:30':false}
 				break;
 		}
 	}
 	hourReservated(hour){
-        if(!this.hours[hour]){
+        if(!this.appointments[hour] && this.appointments[hour] != undefined){
             return false
         }
         return true
@@ -28,8 +29,8 @@ class Day{
 		let mes = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
         return this.date.getDate() + ' de ' + mes[this.date.getMonth()] + ' de ' + this.date.getFullYear()
     }
-	getHours(){
-		return this.hours;
+	getAppointments(){
+		return this.appointments;
 	}
 }
 module.exports = Day;
