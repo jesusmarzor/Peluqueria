8e6e545004854feb586e0f0f4b7a4554f062202b
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-11-06 19:38:52 +0100

    elimino las pruebas que hice en el anterior Objetivo y adapto el codigo para completar las tareas #28 y #29

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ const Service = require('../../Peluqueria/src/Service');
 const Appointment = require('../../Peluqueria/src/Appointment')
 
 class App{
-
 	constructor(){
 		this.calendar = new Calendar(60);
 		this.products = [];
@@ -25,45 +24,20 @@ class App{
 	getServices(){
 		return this.services;
 	}
-
-	cancelAppointment(name,day,hour,service){
-        if(day.hourReservated(hour)){
-            if(day['hours'][hour] == name + ' tiene un ' + service.getName()){
-                day['hours'][hour] = false;
-                console.log(name + " ha anulado su cita");
-            }
-        }else{
-            console.log('\n\nLo siento ' + name + ', el ' + day.getDateString() + ' a las ' + hour + ' no tienes cita')
-        }
-    }
 	giveAppointment(name, day, hour, service){
         if(!day.hourReservated(hour)){
-            new Appointment(service, day, hour, name);
-            console.log(name + " ha reservado cita");
-            day['hours'][hour] = name + ' tiene un ' + service.getName()
-        }else{
-            console.log('\n\nLo siento ' + name + ', el ' + day.getDateString() + ' a las ' + hour + ' ya esta ocupado')
+            day['appointments'][hour] = new Appointment(name, service);
         }
     }
-	getHours(day){
-		return day.getHours();
+	cancelAppointment(name,day,hour,service){
+        if(day.hourReservated(hour)){
+            if(day['appointments'][hour].getCustomerName() == name && day['appointments'][hour].getService() == service){
+                day['appointments'][hour] = false;
+            }
+        }
+    }
+	getAppointments(day){
+		return day.getAppointments();
 	}
 }
-
-const app = new App();
-let calendar = app.getCalendar();
-let products = app.getProducts();
-app.addProducts('acondicionador',5,50);
-products.map((p)=>{
-	console.log(p);
-})
-let services = app.getServices();
-app.createServices('corte',[[products[0],15]],8);
-services.map((s)=>{
-	console.log(s);
-})
-app.giveAppointment('Jesus',calendar.getTimetable()[2],'18:00',services[0]);
-app.giveAppointment('Pablo',calendar.getTimetable()[2],'19:30',services[0]);
-app.cancelAppointment('Pablo',calendar.getTimetable()[2],'19:30',services[0]);
-console.log(app.getHours(calendar.getTimetable()[2]));
-
+module.exports = App;
