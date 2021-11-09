e05476cbbfc5eddd034778ae6fb62f26eb45c250
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-11-09 15:31:39 +0100

    añado a la clase Appointment la hora para resolver el problema #29

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,13 +1,17 @@
 class Appointment{
-	constructor(customerName, service){
+	constructor(customerName, service, hour){
 		this.customerName = customerName;
 		this.service = service;
+		this.hour = hour;
 	}
 	getCustomerName(){
 		return this.customerName;
 	}
 	getService(){
 		return this.service;
+	}
+	getHour(){
+		return this.hour;
 	}
 }
 
