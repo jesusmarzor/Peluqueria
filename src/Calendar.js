c88054e101323bddc3af143ee5872fe987ba5fb4
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-11-09 17:52:32 +0100

    Se cambia la forma de guardar los dias y se ajusta para que se guarden todos los dias del mes en el que estamos, resuelve #30 y #34

--- src/Calendar.js
+++ src/Calendar.js
@@ -3,10 +3,10 @@ class Calendar {
 	constructor(calendarSize){
 		this.calendarSize = calendarSize;
 		this.timetable = [];
-		let currentDate = (new Date());
+		let date = (new Date());
+		date.setDate(date.getDate() - date.getDay());
 		for (let counter = 0; counter < calendarSize; counter++) {
-			currentDate = new Date(currentDate.setDate(currentDate.getDate() + 1));
-			this.timetable.push(new Day(currentDate));
+			this.timetable.push(new Day(new Date(date.setDate(date.getDate()+1))));
 		}
 	}
 	getTimetable(){
