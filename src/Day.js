409d4588adfba2741a3a04637e55dcc25bdb9c80
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-11-09 18:05:27 +0100

    Se cambia el atributo hours por work_hours (depende del dia trabaja unas horas u otras) y se añade un array de Appointments para guardar las citas de cada dia, resuelve #28

--- src/Day.js
+++ src/Day.js
@@ -1,12 +1,14 @@
 class Day{
 	constructor(date){
 		this.date = date;
-		this.hours = {};
+		this.appointments = [];
+		this.work_hours = []
+
 		let dayOfTheWeek = date.getDay();
 
 		switch (dayOfTheWeek) {
 			case 1:
-				this.hours = {'18:00':false,'18:30':false,'19:00':false,'19:30':false,'20:00':false,'20:30':false}
+				this.work_hours = ['18:00','18:30','19:00','19:30','20:00','20:30']
 				break;
 				
 			case 2:
@@ -14,7 +16,7 @@ class Day{
 			case 4:
 			case 5:
 			case 6:
-				this.hours = {'08:00':false,'08:30':false,'09:00':false,'09:30':false,'10:00':false,'10:30':false,'11:00':false,'11:30':false,'12:00':false,'12:30':false,'18:00':false,'18:30':false,'19:00':false,'19:30':false,'20:00':false,'20:30':false}
+				this.work_hours = ['08:00','08:30','09:00','09:30','10:00','10:30','11:00','11:30','12:00','12:30','18:00','18:30','19:00','19:30','20:00','20:30'];
 				break;
 		}
 	}
@@ -32,4 +34,3 @@ class Day{
 		return this.hours;
 	}
 }
-module.exports = Day;
