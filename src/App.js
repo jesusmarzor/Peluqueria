374903f7111095d72ffb2b4cac4fc73423ad92d3
Jesus Martin Zorrilla <jesusmarzor28@gmail.com>
2021-11-09 18:22:58 +0100

    Se modifican los metodos coger cita, cancelar cita para ajustarlo a #28 y #29 y se añade un metodo que te devuelve todas las citas de un dia, resuelve #35

--- src/App.js
+++ src/App.js
@@ -24,28 +24,25 @@ class App{
 	getServices(){
 		return this.services;
 	}
-
-	cancelAppointment(name,day,hour,service){
-        if(day.hourReservated(hour)){
-            if(day['hours'][hour] == name + ' tiene un ' + service.getName()){
-                day['hours'][hour] = false;
-                console.log(name + " ha anulado su cita");
-            }
-        }else{
-            console.log('\n\nLo siento ' + name + ', el ' + day.getDateString() + ' a las ' + hour + ' no tienes cita')
-        }
-    }
 	giveAppointment(name, day, hour, service){
-        if(!day.hourReservated(hour)){
-            new Appointment(service, day, hour, name);
-            console.log(name + " ha reservado cita");
-            day['hours'][hour] = name + ' tiene un ' + service.getName()
-        }else{
-            console.log('\n\nLo siento ' + name + ', el ' + day.getDateString() + ' a las ' + hour + ' ya esta ocupado')
+        if(day.getDate() >= new Date() && !day.hourReservated(hour)){
+            day.addAppointment(new Appointment(name, service,hour));
         }
     }
-	getHours(day){
-		return day.getHours();
+	cancelAppointment(name,day,hour,service){
+        if(day.getDate() >= new Date() && day.hourReservated(hour)){
+			day.getAppointments().map( (appointment,index) => {
+				if(appointment.getCustomerName() == name && appointment.getService() == service && appointment.getHour() == hour){
+					day.cancelAppointment(index);
+				}
+			});
+        }
+    }
+	getAppointments(day){
+		return day.getAppointments();
+	}
+	getAppointment(day, hour){
+		return day.getAppointment(hour);
 	}
 }
-
+module.exports = App;
