4b8af1ba78dcef1314aeddc8016dc6a83b499f76
Jesus Martin <jesusmarzor28@gmail.com>
2021-11-18 21:00:39 +0100

    Se crean variables de clase para crear los identificadores y para contabilizar los beneficios que obtiene la empresa gracias a las citas, resuelve #39 y #41

--- src/Appointment.js
+++ src/Appointment.js
@@ -1,8 +1,16 @@
 class Appointment{
+	static idCounter = 0;
+	static moneyCollected = 0;
 	constructor(customerName, service, hour){
+		this.id = Appointment.idCounter;
 		this.customerName = customerName;
 		this.service = service;
 		this.hour = hour;
+		Appointment.moneyCollected = Appointment.moneyCollected + this.service.getPrice();
+		Appointment.idCounter++;
+	}
+	getId(){
+		return this.id;
 	}
 	getCustomerName(){
 		return this.customerName;
@@ -12,6 +20,12 @@ class Appointment{
 	}
 	getHour(){
 		return this.hour;
+	}
+	static getMoneyCollected(){
+		return Appointment.moneyCollected;
+	}
+	static setMoneyCollected(price){
+		Appointment.moneyCollected -= price;
 	}
 }
 
