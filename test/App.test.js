2a00b6f98edc510844ae24f34328f3300d129d8c
Jesus Martin <jesusmarzor28@gmail.com>
2021-11-18 21:03:49 +0100

    Se ajusta a las buenas practicas para los test, se crean los test para contabilizar gastos y beneficios #41

--- test/App.test.js
+++ test/App.test.js
@@ -1,54 +1,69 @@
 const App = require('../src/App');
-const app = new App();
-const calendar = app.getCalendar();
-const products = app.getProducts();
-const services = app.getServices();
-const day = calendar.getTimetable()[25];
-const hour = '18:00';
-describe('M1 - Sistema de Citas', () => {
+const Product = require('../src/Product');
+const Appointment = require('../src/Appointment')
+
+beforeAll(() => {
+    app = new App();
+    calendar = app.getCalendar();
+    services = app.getServices();
+    day = calendar.getTimetable()[25];
+    hour = '18:00';
+    products = app.getProducts();
+})
+
+describe('Clase Producto', () => {
+    const id_acondicionador = () => app.addProducts('acondicionador',5,50);
     test('Añadir Productos', ()=>{
-        app.addProducts('acondicionador',5,50);
-        expect(products[0].getName()).toBe('acondicionador');
-        expect(products[0].getStock()).toBe(5);
-        expect(products[0].getPrice()).toBe(50);
-        expect(products.length).toBe(1);
+        let id_product = () => products[0].getId();
+        expect(id_acondicionador()).toBe(id_product());
     })
+    test('Dinero gastado en productos', () => {
+        let money = Product.getMoneySpent();
+        expect(money).toBe(50);
+        let id_champu = () => app.addProducts('champu',5,20);
+        id_champu();
+        money = Product.getMoneySpent();
+        expect(money).toBe(70);
+    })
+})
+describe('Clase Servicio', () => {
+    const id_corte = () => app.createServices('corte',[[products[0],15]],8);
     test('Añadir Servicios', () => {
-        app.createServices('corte',[[products[0],15]],8);
-        expect(services[0].getName()).toBe('corte');
-        expect(services[0].getConsumption()).toEqual([[products[0],15]]);
-        expect(services[0].getPrice()).toBe(8);
-        expect(services.length).toBe(1);
+        let id_service = () => services[0].getId();
+        expect(id_corte()).toBe(id_service());
     })
+})
+describe('Clase Appointment', () => {
+    const id = () => app.giveAppointment('Jesus',day,hour,services[0]);
+    const id_close = () => app.giveAppointment('Juan',day,'22:00',services[0]);
+    const id_busy = () => app.giveAppointment('Juan',day,hour,services[0]);
     test('Pedir cita', () => {
-        app.giveAppointment('Jesus',day,hour,services[0]);
-        let appointmentGiven = app.getAppointment(day,hour);
-        expect(appointmentGiven.getCustomerName()).toBe('Jesus');
-        expect(appointmentGiven.getService()).toEqual(services[0]);
-        expect(appointmentGiven.getHour()).toEqual(hour);
+        let appointmentGiven = () => app.getAppointment(day,0);
+        expect(id()).toBe(appointmentGiven().getId());
     })
     test('Pedir cita a una hora que está cerrado', () => {
-        app.giveAppointment('Juan',day,'22:00',services[0]);
-        let appointmentGiven = app.getAppointment(day,'22:00');
-        expect(appointmentGiven).toBe(undefined);
+        expect(id_close()).toBe(undefined);
     })
     test('Pedir cita a una hora que está ocupada', () => {
-        app.giveAppointment('Juan',day,hour,services[0]);
-        let appointmentGiven = app.getAppointment(day,hour);
-        expect(appointmentGiven.getCustomerName()).toBe('Jesus');
-        expect(appointmentGiven.getService()).toEqual(services[0]);
-        expect(appointmentGiven.getHour()).toEqual(hour);
+        expect(id_busy()).toBe(undefined);
     })
-    test('Anular cita que no es tuya', () => {
-        app.cancelAppointment('Juan',day,hour,services[0]);
-        let appointmentCancel = app.getAppointment(day,hour);
-        expect(appointmentCancel.getCustomerName()).toBe('Jesus');
-        expect(appointmentCancel.getService()).toEqual(services[0]);
-        expect(appointmentCancel.getHour()).toEqual(hour);
+    test('Dinero ganado con las citas', () => {
+        let money = Appointment.getMoneyCollected();
+        expect(money).toBe(8)
+        let appointmentGiven = () => app.giveAppointment('Pepe',day,'19:00',services[0]);
+        appointmentGiven()
+        money = Appointment.getMoneyCollected();
+        expect(money).toBe(16)
     })
     test('Anular cita', () => {
-        app.cancelAppointment('Jesus',day,hour,services[0]);
-        let appointmentCancel = app.getAppointment(day,hour);
-        expect(appointmentCancel).toBe(undefined);
+        let cancelAppointment = () => {
+            app.cancelAppointment(day,0)
+            return app.getAppointment(day,0)
+        };
+        expect(cancelAppointment()).toBe(undefined);
+    })
+    test('Dinero ganado al anular una cita', () => {
+        let money = Appointment.getMoneyCollected();
+        expect(money).toBe(8)
     })
 })
