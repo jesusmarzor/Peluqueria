a30c84b90610787898acc260f23fdc6735e321fd
Jesús Martín Zorrilla <82895619+jesusmarzor@users.noreply.github.com>
2021-12-22 16:47:54 +0100

    Corregir indentación

--- src/utils/Configuration.js
+++ src/utils/Configuration.js
@@ -4,16 +4,16 @@ const client = new Etcd3();
 
 class Configuration {
 	constructor(){
-    this.logDir = null;
-    this.logFile = null;
-    (async () => this.logDir = await client.get('LOG_DIR').string().catch( err => console.log('Log_Dir', err.message)))();
-    if(this.logDir === null){
-      this.logDir = (process.env.LOG_DIR || '/tmp/logs/peluqueria/');
-    }
-    (async () => this.logFile = await client.get('LOG_FILE').string().catch( err => console.log('Log_File', err.message)))();
-    if(this.logFile === null){
-      this.logFile = (process.env.LOG_FILE || 'file.log');
-    }
+	    this.logDir = null;
+	    this.logFile = null;
+	    (async () => this.logDir = await client.get('LOG_DIR').string().catch( err => console.log('Log_Dir', err.message)))();
+	    if(this.logDir === null){
+	      this.logDir = (process.env.LOG_DIR || '/tmp/logs/peluqueria/');
+	    }
+	    (async () => this.logFile = await client.get('LOG_FILE').string().catch( err => console.log('Log_File', err.message)))();
+	    if(this.logFile === null){
+	      this.logFile = (process.env.LOG_FILE || 'file.log');
+	    }
 	}
 
   getLogDir(){
