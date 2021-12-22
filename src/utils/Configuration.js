fb7842fdfd2dbeddd54835718261fabccc9ef3f8
Jesus Martin <jesusmarzor28@gmail.com>
2021-12-22 16:38:13 +0100

    Añadir path a dotenv

--- src/utils/Configuration.js
+++ src/utils/Configuration.js
@@ -1,13 +1,12 @@
 const { Etcd3 } = require('etcd3');
-require('dotenv').config();
-
+require('dotenv').config({ path: './configuration.env'});
 const client = new Etcd3();
 
 class Configuration {
 	constructor(){
     this.logDir = null;
     this.logFile = null;
-		(async () => this.logDir = await client.get('LOG_DIR').string().catch( err => console.log('Log_Dir', err.message)))();
+    (async () => this.logDir = await client.get('LOG_DIR').string().catch( err => console.log('Log_Dir', err.message)))();
     if(this.logDir === null){
       this.logDir = (process.env.LOG_DIR || '/tmp/logs/peluqueria/');
     }
